3396a1b295ca821afbf4a19d4bc5d8aa6f3fb44a
Luja von Köckritz <luvo4837@student.su.se>
2023-03-02 17:48:20 +0100

    Moved files, started network grid development

--- script/Base_Model.py
+++ script/Base_Model.py
@@ -1,8 +1,19 @@
 #%%
-import mesa
+import random
 import pandas as pd
+import networkx as nx
 from scipy.stats import truncnorm
 
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import NetworkGrid
+#from mesa.datacollection import DataCollector
+
+#import networkx as nx
+
+#Parameters
+prob = 0.5
+num_agents = 10
 
 #Group data
 el_prop = 0.195              #TODO: add this to a config section
@@ -12,28 +23,29 @@ rt_prop = 0.146
 clusterData = pd.read_csv("../data/processed/Cluster_data.CSV", delimiter=';', index_col = [0], header=[0,1])
 
 
-class UserAgent(mesa.Agent):
+class UserAgent(Agent):
     def __init__(self, unique_id, model, group):
        super().__init__(unique_id, model)
        self.group = group 
+       self.ice_attitude = self.truncated_normal(clusterData.loc['ICE',(self.group,'Mean')], clusterData.loc['ICE',(self.group,'SD')], 0, 10)         #TODO: check if bounds are correct
+          
+    def truncated_normal(self, mean, std, lower_bound, upper_bound):
+        return truncnorm.rvs(
+            (lower_bound - mean) / std, (upper_bound - mean) / std, loc=mean, scale=std
+        )
 
     def step(self):
-        print("Hi, I am agent " + str(self.unique_id) + " and I am in group: " + self.group)
-
-    #def truncated(self, mean, std, lower_bound, upper_bound):
-    #    return truncnorm.rvs(
-    #        (lower_bound - mean) / std, (upper_bound - mean) / std, loc=mean, scale=std
-    #    )
+        print("Agent " + str(self.unique_id) + "  in group: " + self.group + " my ICE attitude: " + str(self.ice_attitude))
 
 
-class TransportModel(mesa.Model):
+class TransportModel(Model):
     def __init__(self, N):
         self.num_agents = N
-        self.schedule = mesa.time.RandomActivation(self)
-        #self.grid = mesa.grid.NetworkGrid({})
+        self.schedule = RandomActivation(self)
+        #self.G = nx.erdos_renyi_graph(n=self.num_agents, p=prob)
+        self.grid = NetworkGrid(self.G)
         self.create_agents()
         
-
     def create_agents(self):
         for i in range(self.num_agents):
             if i < self.num_agents * el_prop:
@@ -47,13 +59,27 @@ class TransportModel(mesa.Model):
 
             agent = UserAgent(i, self, group)
             self.schedule.add(agent)
-            #self.grid.add_node(agent)
+            self.grid.place_agent(agent)
+
+         # Create edges between agents
+        for agent in self.schedule.agents:
+            for other in self.schedule.agents:
+                if agent != other:
+                    if agent.group == other.group:
+                        if random.random() < prob:
+                            self.grid.add_edge(agent, other)            #TODO: add weight generation
+                    else:
+                        if random.random() < prob * 0.1:
+                            self.grid.add_edge(agent, other)
     
     def step(self):
         """Advance the model by one step."""
         self.schedule.step()
 
-#%%
-empty_model = TransportModel(10)
+
+empty_model = TransportModel(num_agents)
 empty_model.step()
-#%%
+
+
+
+# %%
